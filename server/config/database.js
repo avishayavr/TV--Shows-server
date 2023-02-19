e5aa77865db7c232528a3633a234be38b82a1303
avishay avraham <avishayavraham555@gmail.com>
2023-02-19 21:42:45 +0200

    installing packages

---
+++ server/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://avishayav:mymdbaccount99@cluster0.mlivppk.mongodb.net/showsDB?retryWrites=true&w=majority", () => {
+    console.log("Connected to Database")
+})
+// mongoose.connect("mongodb://localhost:27017/ShowsDB", () => {
+//     console.log("Connected to Database")
+// })
+
+
+  
